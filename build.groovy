5b363c982db834649471d03d67f374c76980dc0d
dvdrbn <12785291+dvdrbn@users.noreply.github.com>
2021-01-10 01:09:47 +0200

    Update build.groovy

--- build.groovy
+++ build.groovy
@@ -1,13 +1,5 @@
 node("linux") {
     customImage = ""
-    stage("create dockerfile") {
-        sh """
-            tee Dockerfile <<-'EOF'
-              FROM ubuntu:latest
-              RUN touch file-01.txt
-EOF
-        """
-    }
     stage("build docker") {
         customImage = docker.build("dvdrbn/opsschool-jenkins-test-01")
     }
