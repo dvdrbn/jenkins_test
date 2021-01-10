a793b3a166605f89ea37b610a175520ee9c00308
dvdrbn <12785291+dvdrbn@users.noreply.github.com>
2021-01-10 02:44:24 +0200

    Update build.groovy

--- build.groovy
+++ build.groovy
@@ -1,14 +1,14 @@
 node("linux") {
-    CMD = "curl --write-out %{http_code} --silent --output /dev/null localhost:80"
+    imageName = "dvdrbn/opsschool-jenkins-test-01"
     customImage = ""
     stage("Clone repo"){
         checkout scm
     }
     stage("Build image") {
-        customImage = docker.build("dvdrbn/opsschool-jenkins-test-01")
+        customImage = docker.build(imageName)
     }
     stage("Test container") {
-        docker.image(customImage).withRun{
+        docker.image(imageName).withRun{
             timeout(time: 15, unit: 'SECONDS') {
                 waitUntil{
                     try {         
